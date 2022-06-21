f6f746cd9352a369dee9ac1b40e2fb1e96e8482a
kavlevru <i@kavlev.ru>
2022-06-21 20:49:31 +0300

    Add lessons 4.1-4.3  from 4 path

---
+++ 2022/algoritms/06/sum_function.py
@@ -0,0 +1,18 @@
+def sum(array):
+    if array == []:
+        return 0
+    x = array.pop()
+    return x + sum(array)
+
+def array_count(array):
+    if array == []:
+        return 0
+    return 1 + array_count(array[1:])
+
+def max(array):
+    if len(array) == 2:
+        return array[0] if array[0] > array[1] else array[1]
+    sub_max = max(array[1:])
+    return array[0] if array[0] > sub_max else sub_max
+
+print(max([2, 4, 6, 10, 11, 14]))
